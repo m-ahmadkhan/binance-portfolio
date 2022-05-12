313ad9bd0cd2976de7e725760882e4cfade950da
Ahmad Khan <ahmad.khan@educative.io>
2022-05-12 23:06:28 +0500

    typescript fixes

--- app/src/screens/Main.tsx
+++ app/src/screens/Main.tsx
@@ -1,14 +1,6 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { Text, View, Button } from 'react-native';
 import Routes from '../constants/routes';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 const Main = ({ navigation }) => {
   return (
